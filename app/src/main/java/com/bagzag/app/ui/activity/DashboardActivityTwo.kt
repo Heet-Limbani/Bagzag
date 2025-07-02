fe4600e2076559748b61c2d03756a11deb3d6115
Heet-hlis <heet.hyperlinkinfosystem@gmail.com>
2025-07-02 18:33:24 +0530

    Search 2

---
+++ app/src/main/java/com/bagzag/app/ui/activity/DashboardActivityTwo.kt
@@ -0,0 +1,37 @@
+package com.bagzag.app.ui.activity
+
+import android.os.Bundle
+import android.view.View
+import com.bagzag.app.R
+import com.bagzag.app.databinding.DashboardAcitivtyBinding
+import com.bagzag.app.ui.base.BaseActivity
+import com.bagzag.app.ui.base.BaseFragment
+import com.bagzag.app.ui.manager.ActivityStarter
+import com.bagzag.app.utils.serializable
+
+class DashboardActivityTwo : BaseActivity() {
+
+    private lateinit var dashboardActivityBinding: DashboardAcitivtyBinding
+
+    override fun findFragmentPlaceHolder(): Int {
+        return R.id.placeHolder
+    }
+
+    override fun createViewBinding(): View {
+        dashboardActivityBinding = DashboardAcitivtyBinding.inflate(layoutInflater)
+        return dashboardActivityBinding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            val page: Class<BaseFragment<*>> =
+                intent.serializable(ActivityStarter.ACTIVITY_FIRST_PAGE)
+            load(page)
+                .setBundle(intent.extras!!)
+                .replace(false)
+        }
+    }
+
+}
